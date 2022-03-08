4a2769a6d1d27f430f8b0bc554e436edda94dc97
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-08 17:46:19 -0400

    Requirement 1 Complete! :heavy_check_mark: Array that returns the average of the values

--- src/average.js
+++ src/average.js
@@ -12,8 +12,30 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  // Verifica se é um array vazio primeiro
+  if (typeof array[0] === 'undefined') return undefined;
+  for (let index of array) {
+    // Verifica se os valores do array são númericos
+    if (typeof index !== 'number') {
+      return undefined;
+    }
+  }
+  let soma = 0;
+  let media = 0;
+  // Soma os valores do array
+  for (let index of array) {
+    soma += index;
+  }
+  // Calcula a média dos valores recebidos dividindo pelo tamanho do array
+  media = soma / array.length;
+  // Arredondando os valores da média
+  // Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+  media = media.toFixed();
+  // Convertendo o valor em string para inteiro
+  return parseFloat(media);
 };
+
+// console.log(typeof average([]));
 
 module.exports = average;
