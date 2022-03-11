f6f8f2f12cf6aa6b8cecbaaee5f3388386d317c2
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-11 18:22:50 -0400

    Requirement 4 Complete! :heavy_check_mark: Implemented tests for cicle function

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,27 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle(typeof radius !== 'number')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(typeof (circle(3))).toBe('object');
     // Teste se o objeto retornado tem 3 propriedades.
+    // Source: https://pt.stackoverflow.com/questions/15058/como-saber-o-tamanho-quantidade-de-propriedades-atributos-de-um-objeto-em-ja
+    // Obejct.keys é como se fosse um for/in, percorre o objeto e retorna um array
+    // Nesse caso, usar circle.length não daria certo sem o Object.keys pq circle é objeto e não array ;)
+    expect(Object.keys(circle(3)).length).toBe(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
+    expect(circle(2).circumference).toBe(12.56);
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    // Source: https://jestjs.io/pt-BR/docs/expect#tobeclosetonumber-numdigits
+    expect(circle(3).area).toBeCloseTo(28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3).radius).toBe(3);
+    expect(circle(3).area).toBeCloseTo(28.26);
+    expect(circle(3).circumference).toBe(18.84);
   });
 });
